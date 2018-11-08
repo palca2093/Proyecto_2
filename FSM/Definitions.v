ebef25e5b73da41abb54584128af53ae1a8b3c92
Pablo A <palca2093@gmail.com>
2018-11-08 15:58:53 -0600

    FSM

---
+++ FSM/Definitions.v
@@ -0,0 +1,11 @@
+`ifndef Definitions_v
+`define Definitions_v
+
+`default_nettype none
+`define RESET    5'b00001
+`define INIT     5'b00010
+`define IDLE     5'b00100
+`define ACTIVE   5'b01000
+`define ERROR    5'b10000
+
+`endif
